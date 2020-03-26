ea6c10623defca4671f779182cdac8bc8303c43f
itdev-lenovo <it1devops@gmail.com>
2020-03-26 13:06:46 +0530

    files added

---
+++ src/main/java/com/java/errorhandling/Error.java
@@ -0,0 +1,13 @@
+package com.java.errorhandling;
+
+public class Error {
+/*
+ * Write the program based on exception and using throws keyword in java 
+   and Write the program using the following exceptions in java 
+	a.	ArrayIndexOutOfBound
+	b.	NumberFormatException
+	c.	Try-catch
+
+
+ */
+}
